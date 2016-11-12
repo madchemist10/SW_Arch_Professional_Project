137a5cd3b1bbbc0fdcb96e77ba453e2588132783
hds109 <hds109@msstate.edu>
2016-11-12 17:44:37 -0600

    Fixed coding structure for accurate function calls

--- Development/src/app/utilities/apiHandlers/TradierAPIHandler.java
+++ Development/src/app/utilities/apiHandlers/TradierAPIHandler.java
@@ -19,14 +19,43 @@ class TradierAPIHandler extends AAPIHandler{
 
     private TradierAPIHandler(){
         super();
-        getTradier();
     }
 
-    private static JsonNode getTradier(){
-            BufferedReader responseBody = null;
-            JsonNode jsonObj = null;
-            //build client
-            HttpClient tradierClient = HttpClientBuilder.create().build();
+    /**
+     * Retrieve an instance of the TradierAPIHandler so that
+     * the application may have access to the methods within
+     * this class.
+     * Instance is a singleton reference.
+     * @return instance of this TradierAPIHandler.
+     */
+    static AAPIHandler getInstance(){
+        if(instance == null){
+            instance = new TradierAPIHandler();
+        }
+        return instance;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String buildAPIRequest(String[] inputs) {
+        return null;
+    }
+
+    /**
+     * Handles the execution of an API request to the Tradier API.
+     * The return type for this execution is {@link JsonNode}.
+     * The calling method should cast the object returned from this
+     * execution to {@link JsonNode}.
+     * {@inheritDoc}
+     */
+    @Override
+    public Object executeAPIRequest(String request){
+        BufferedReader responseBody = null;
+        JsonNode jsonObj = null;
+        //build client
+        HttpClient tradierClient = HttpClientBuilder.create().build();
 
         try {
             //Form request
@@ -67,27 +96,6 @@ class TradierAPIHandler extends AAPIHandler{
             }
         }
         return jsonObj;
-    }
 
-    /**
-     * Retrieve an instance of the TradierAPIHandler so that
-     * the application may have access to the methods within
-     * this class.
-     * Instance is a singleton reference.
-     * @return instance of this TradierAPIHandler.
-     */
-    static AAPIHandler getInstance(){
-        if(instance == null){
-            instance = new TradierAPIHandler();
-        }
-        return instance;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public String buildAPIRequest(String[] inputs) {
-        return null;
     }
 }
