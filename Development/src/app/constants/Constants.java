8057ec0c8721dd368de03c0a17868a02a978a05e
madchemist10 <2012kemp1234@gmail.com>
2016-11-12 13:55:30 -0600

    Update to allow for user specified settings. This is needed for use with authentication on APIs.

--- Development/src/app/constants/Constants.java
+++ Development/src/app/constants/Constants.java
@@ -8,20 +8,23 @@ public class Constants {
     /*Twitter API Access Credentials*/
     /**Consumer key for the Twitter API Calls.*/
     public static final String TWITTER_API_CONSUMER_KEY =
-            "qZs4bQKHov2dHVsNwtATsKbyr";
+            "TWITTER_API_CONSUMER_KEY";
 
     /**Consumer secret for the Twitter API Calls.*/
     public static final String TWITTER_API_CONSUMER_SECRET =
-            "mGj0bWBazA5dabYz8vz3AxT8d7vUD8jziXNeKKJsw3YJMCQPmb";
+            "TWITTER_API_CONSUMER_SECRET";
 
     /**Access token for the Twitter API Calls.*/
     public static final String TWITTER_API_ACCESS_TOKEN =
-            "796483597335044096-dwzJYOeoP3WxVhlELEhW7CNiiEYNUPJ";
+            "TWITTER_API_ACCESS_TOKEN";
 
     /**Access token secret for the Twitter API Calls.*/
     public static final String TWITTER_API_ACCESS_TOKEN_SECRET =
-            "EgIOB5kWi8On63c1K9h61fr7gO6DTubZxV2Cwgjjd0KvX";
+            "TWITTER_API_ACCESS_TOKEN_SECRET";
 
     /**Identifier for MD5 algorithm.*/
     public static final String MD5_ALGORITHM = "MD5";
+
+    /**Local path for the settings file.*/
+    public static final String SETTINGS_FILE = "settings.txt";
 }
